b5abeaa7051c6788d2db1a1db040bd224b475e4d
rakulan-srinivasan <vsriabilarag@live.com>
2023-04-08 22:21:34 +0530

    Worked on CSS

--- templates/script.js
+++ templates/script.js
@@ -1,9 +1,8 @@
-const mySlider = document.getElementById("my-slider");
-const sliderValue = document.getElementById("slider-value");
-
-function slider(){
-    valPercent = mySlider.value;
+function slider(get){
+    const mySlider = document.getElementById("my-slider");
+    const sliderValue = document.getElementById("slider-value");
+    var valPercent = mySlider.value;
+    console.log(valPercent);
     mySlider.style.background = `linear-gradient(to right, #3264fe ${valPercent}%, #d5d5d5 ${valPercent}%)`;
     sliderValue.textContent = mySlider.value;
-}
-slider();
+};
