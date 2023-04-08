abf1f7fe8c75b9e4d86b944aef74852f45a57fb1
rakulan-srinivasan <vsriabilarag@live.com>
2023-04-08 22:46:47 +0530

    Debugged app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     print("came in")
-    age = float(request.form['age'])
+    age = request.form.get('age')
     print("age")
-    person_home_ownership = request.form['person_home_ownership']
+    person_home_ownership = request.form.get('person_home_ownership')
     print(person_home_ownership)
     if person_home_ownership=='Rent':
         person_home_ownership=3
@@ -31,12 +31,12 @@ def predict():
         person_home_ownership=0
     else:
         person_home_ownership=1
-    person_income = float(request.form['person_income'])
+    person_income = float(request.form.get('person_income'))
     print(person_income)
-    person_emp_length = float(request.form['person_emp_length'])
+    person_emp_length = float(request.form.get('person_emp_length'))
     print("Emp len")
     print(person_emp_length)
-    loan_intent = request.form['loan_intent']
+    loan_intent = request.form.get('loan_intent')
     print(loan_intent)
     if loan_intent=='PERSONAL':
         loan_intent=4
@@ -50,20 +50,32 @@ def predict():
         loan_intent=5
     else:
         loan_intent=0
-
-    loan_grade = float(request.form['loan_grade'])
+    loan_grade = request.form.get('loan_grade')
     print(loan_grade)
-    loan_amnt = float(request.form['loan_amnt'])
+    if loan_grade == 'A':
+        loan_grade = 0
+    elif loan_grade == 'B':
+        loan_grade = 1
+    elif loan_grade == 'C':
+        loan_grade = 2
+    elif loan_grade == 'D':
+        loan_grade = 3
+    elif loan_grade == 'E':
+        loan_grade = 4
+    loan_amnt = float(request.form.get('loan_amnt'))
     print(loan_amnt)
-    loan_int_rate = float(request.form['loan_int_rate'])
+    loan_int_rate = float(request.form.get('loan_int_rate'))
     print(loan_int_rate)
-    loan_percent_income = float(request.form['loan_percent_income'])
+    loan_percent_income = float(request.form.get('loan_percent_income'))
     print("Loan percent")
     print(loan_percent_income)
-    cb_person_default_on_file = float(request.form['cb_person_default_on_file'])
-    print("cb_default")
+    cb_person_default_on_file = request.form.get('cb_person_default_on_file')
+    if cb_person_default_on_file == 'N':
+        cb_person_default_on_file = 0
+    elif cb_person_default_on_file == 'Y':
+        cb_person_default_on_file = 1
     print(cb_person_default_on_file)
-    cb_person_cred_hist_length = float(request.form['cb_preson_cred_hist_length'])
+    cb_person_cred_hist_length = float(request.form.get('cb_preson_cred_hist_length'))
     print(cb_person_cred_hist_length)
 
     data = np.array([
